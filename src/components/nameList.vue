f7bfb1431e232abc0a168494a7cab1f32dbd1e9d
Dani Vijay <danimvijay@gmail.com>
2017-12-31 20:26:07 +0530

    add nameList

---
+++ src/components/nameList.vue
@@ -0,0 +1,17 @@
+<template>
+<h1>hello world</h1>
+</template>
+
+<script>
+export default {
+  name: 'nameList',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
